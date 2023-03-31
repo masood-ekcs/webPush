d8e4b68bed37eb4a94b8ccfda66007762da6ea09
masoodcodingzen <masood.codingzen@gmail.com>
2023-03-31 11:06:05 +0530

    M

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const webPush = require('web-push')
+const bodyParser = require('body-parser') // used to send data to the frontend using a form action based page. (without fetch)
+const webPush = require('web-push') // used for notifications
 const app = express()
 
 
 
 app.use(bodyParser.json())
 app.use(express.static(__dirname + "/public"))
+// In the backend, a file cannot send a response to another file.
+// However, we can send files through a STATIC folder. Here, we have created a PUBLIC folder as our Static folder. So, we’ll move all the dependent files, script.js and style.css files in a folder called, ‘PUBLIC’ and will update our code.
+
 app.set("view engine", "ejs")
 app.get("/", (req, res) => {
     res.render("index")
@@ -24,19 +27,19 @@ app.post("/data", (req, res) => {
 })
 
 
-
+// These keys will help us to Register at Client’s end…
+// We will save these keys in a variable to use it in our application…
+// For webPush, we HAVE to use these mathematically generated keys ONLY. We cannot use any other keys.
 const public_key = "BJfT0VtK77zmrMP1BXVs2Zi1-NjwkjUZoEzzCvSGJYR1P2AhyKml-KypPZO_WZzoqed-281a93dSR2VUsPkV4oA"
 const private_key = "thBDfAUEe1Te5O_bmxVjh6ma_mirHtYGgcJp9p9wKkc"
 
 
-
+// with the help of webPush, we'll set these keys
+// Publisher/Developer's email ID – used to send future email by the Browser, in case of any issue
 webPush.setVapidDetails('mailto:kryptonites.ju@gmail.com', public_key, private_key)
 
 
 // ≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠ S E R V E R
-const port = 3000
-// const port = process.env.PORT || 3000
+// const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, (err) => { (err) ? console.log(err) : console.log("Server is running on http://localhost:" + port) })
-
-
-
